4a566f10710c10ec5fa73773ccdc3d3a640d707d
cyptrix12 <120047588+cyptrix12@users.noreply.github.com>
2025-03-30 23:40:09 +0200

    Update GameController.py

--- src/GameController.py
+++ src/GameController.py
@@ -20,6 +20,7 @@ class GameController:
         self.active_enemies = 0
         self.endless_runner = False  
         self.w_pressed = False
+        self.pressed_keys = set()
         self.money = 100 
         self.scene.update_money(self.money)
         self.game_over = False
@@ -28,36 +29,35 @@ class GameController:
         if self.game_over:
             return True
         if event.type() == QEvent.KeyPress:
-            if event.key() == Qt.Key_W:
-                self.w_pressed = True
+            self.pressed_keys.add(event.key())
             if event.key() == Qt.Key_Space:
                 self.scene.start_button.click()
         elif event.type() == QEvent.KeyRelease:
-            if event.key() == Qt.Key_W:
-                self.w_pressed = False
+            self.pressed_keys.discard(event.key())
         if event.type() == QEvent.GraphicsSceneMousePress:
             if event.button() == Qt.LeftButton:
                 return self.handle_mouse_event(event)
         return False
 
     def handle_mouse_event(self, event):
-        if not self.w_pressed:
-            return False
+        # if Qt.Key_W in self.pressed_keys:
+        #     return False
         pos = event.scenePos()
         clicked_items = self.scene.items(pos) 
 
-        for item in clicked_items:
-            if isinstance(item, AnimatedTower):
-                if self.money < 10:
-                    print("Not enough money to upgrade the tower!")
-                    return False
+        if len(clicked_items) != 0:
+            for item in clicked_items:
+                if isinstance(item, AnimatedTower) and Qt.Key_W in self.pressed_keys:
+                    if self.money < 10:
+                        print("Not enough money to upgrade the tower!")
+                        return False
 
-                self.money -= 10
-                self.scene.update_money(self.money)
+                    self.money -= 10
+                    self.scene.update_money(self.money)
 
-                item.upgrade()
-                print(f"Tower upgraded! New damage: {item.damage}, new range: {item.range}")
-                return True
+                    item.upgrade()
+                    print(f"Tower upgraded! New damage: {item.damage}, new range: {item.range}")
+                    return True
 
         x = int(pos.x() // self.GRID_SIZE)
         y = int(pos.y() // self.GRID_SIZE)
@@ -71,10 +71,12 @@ class GameController:
             if self.money < 20:
                 print("Not enough money!")
                 return False
-            self.money -= 20
-            self.scene.update_money(self.money)
-            self.addTower(x, y)
-            return False
+            if self.addTower(x, y):
+                self.money -= 20
+                self.scene.update_money(self.money)
+                return True
+            else:
+                return False
         else:
             self.addEnemy()
             return True
@@ -82,17 +84,18 @@ class GameController:
 
     def addTower(self, x, y):
         if (x, y) in self.scene.tower_positions:
-            return
+            return False
         if (x, y) in self.scene.path:
-            return
+            return False 
         for overlay in self.scene.overlay_items:
             if (x, y) == overlay["pos"]:
-                return
+                return False
         if x < 0 or y < 0 or x >= self.scene.GRID_WIDTH or y >= self.scene.GRID_HEIGHT:
-            return
+            return False
         tower = AnimatedTower(x, y, self.scene)
         self.scene.addItem(tower)
         self.scene.tower_positions.add((x, y))
+        return True
 
     def addEnemy(self):
         enemy = AnimatedEnemy(self.scene.path[0][0], self.scene.path[0][1], self.scene.path, self.scene, self)
